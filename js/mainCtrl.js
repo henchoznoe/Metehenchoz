5801bedb317bd9a76e790d8cda349d02958142aa
HenchozN <nhenchoz122@icloud.com>
2022-05-26 13:37:59 +0200

    Noé

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -23,3 +23,388 @@ function lol() {
 function reloadPage() {
     window.location.reload(true);
 }
+function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}function test(city) {
+    $.ajax('https://api.weatherapi.com/v1/current.json', {
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            key: 'b301c07a26014a9cbb355734222505',
+            q: city,
+            aqi: 'no'
+        },
+        success: function (data) {
+            console.log(data.location)
+            document.getElementById('test').innerHTML = data.location.country;
+
+        }
+    });
+}
+
+function lol() {
+    console.log($("#abc").html());
+    test($("#abc").html());
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}
