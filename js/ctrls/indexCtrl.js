5d0d81d21d3aae82734182c529ec46244c73fc84
HenchozN <nhenchoz122@icloud.com>
2022-06-01 09:36:23 +0200

    Noé

--- js/ctrls/indexCtrl.js
+++ js/ctrls/indexCtrl.js
@@ -11,19 +11,17 @@ $().ready(function () {
 });
 
 class IndexCtrl {
-
   constructor() {
     this.chargerIcon();
     this.loadHome();
     this.loadFooter();
     this.loadEvents();
   }
-  
 
   chargerIcon() {
     let heure = new Date().getHours();
     if (heure > 8 && heure < 20) {
-      document.getElementById('icon').href = 'img/sun.jpg';
+      document.getElementById("icon").href = "img/sun.jpg";
     }
   }
 
@@ -32,86 +30,82 @@ class IndexCtrl {
   }
 
   hideNavCollapsed() {
-    let nav = document.getElementById('navbarResponsive');
-    if (nav.className.includes('show')) {
-      nav.classList.remove('show');
+    let nav = document.getElementById("navbarResponsive");
+    if (nav.className.includes("show")) {
+      nav.classList.remove("show");
     }
   }
 
   loadHome() {
-    httpServ.chargerVue('home', function () {
+    httpServ.chargerVue("home", function () {
       new HomeCtrl();
     });
   }
 
   loadStations() {
-    httpServ.chargerVue('stations', function () {
+    httpServ.chargerVue("stations", function () {
       new StationsCtrl();
     });
   }
 
   loadFooter() {
-    httpServ.chargerFooter('footer', function () {
+    httpServ.chargerFooter("footer", function () {
       new FooterCtrl();
     });
   }
 
   loadAbout() {
-    httpServ.chargerVue('about', function () {
+    httpServ.chargerVue("about", function () {
       new AboutCtrl();
     });
   }
 
-  loadEvents(){
-    $('#a-home').click(function () {
+  loadEvents() {
+    $("#a-home").click(function () {
       indexCtrl.hideNavCollapsed();
-      $('a.nav-link').removeClass('active');
+      $("a.nav-link").removeClass("active");
       indexCtrl.loadHome();
     });
-    $('#a-stations').click(function () {
+    $("#a-stations").click(function () {
       indexCtrl.hideNavCollapsed();
-      $('a.nav-link').removeClass('active');
-      $('#a-stations').addClass('active');
+      $("a.nav-link").removeClass("active");
+      $("#a-stations").addClass("active");
       indexCtrl.loadStations();
     });
-    $('#a-about').click(function () {
+    $("#a-about").click(function () {
       indexCtrl.hideNavCollapsed();
-      $('a.nav-link').removeClass('active');
-      $('#a-about').addClass('active');
+      $("a.nav-link").removeClass("active");
+      $("#a-about").addClass("active");
       indexCtrl.loadAbout();
     });
-    $('#nav-in-search').keypress(function () {
-      console.log($('#nav-in-search').val());
+    $("#nav-in-search").keypress(function () {
+      console.log($("#nav-in-search").val());
     });
-    $('#nav-btn-search').click(function () {
-      let value = $('#nav-in-search').val();
-      $('#nav-in-search').val('');
-      alert('Recherche de ' + value + ' en cours...');
-    })
-
+    $("#nav-btn-search").click(function () {
+      let value = $("#nav-in-search").val();
+      $("#nav-in-search").val("");
+      alert("Recherche de " + value + " en cours...");
+    });
   }
 
-
-
   api(city) {
-    $.ajax('https://api.weatherapi.com/v1/current.json', {
-      type: 'GET',
-      contentType: 'application/json',
+    $.ajax("https://api.weatherapi.com/v1/current.json", {
+      type: "GET",
+      contentType: "application/json",
       data: {
-        key: 'b301c07a26014a9cbb355734222505',
+        key: "b301c07a26014a9cbb355734222505",
         q: city,
-        aqi: 'no',
+        aqi: "no",
       },
       success: function (data) {
-        document.getElementById('api-text').innerHTML = data.location.country;
-        document.getElementById('api-text').innerHTML += ' / ' + data.location.region;
+        document.getElementById("api-text").innerHTML = data.location.country;
+        document.getElementById("api-text").innerHTML +=
+          " / " + data.location.region;
       },
     });
   }
 
   find() {
-    this.api(document.getElementById('api-input').value);
+    this.api(document.getElementById("api-input").value);
   }
-
-  
 }
