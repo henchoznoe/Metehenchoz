7f20f6e829223b09a0f63eee16fb2192597bcf91
HenchozN <nhenchoz122@icloud.com>
2022-06-13 12:52:12 +0200

    Noé

--- js/ctrls/homeCtrl.js
+++ js/ctrls/homeCtrl.js
@@ -5,39 +5,66 @@
 */
 
 class HomeCtrl {
-
   constructor() {
     this.events();
-    this.loadWeather(HOME_CITY1, '#title-city1', '#icon1', '#temp-city1', HOME_CITY1_TIMEZONE);
-    this.loadWeather(HOME_CITY2, '#title-city2', '#icon2', '#temp-city2', HOME_CITY2_TIMEZONE);
-    this.loadWeather(HOME_CITY3, '#title-city3', '#icon3', '#temp-city3', HOME_CITY3_TIMEZONE);
+    this.loadWeather(
+      HOME_CITY1,
+      "#title-city1",
+      "#icon1",
+      "#temp-city1",
+      HOME_CITY1_TIMEZONE
+    );
+    this.loadWeather(
+      HOME_CITY2,
+      "#title-city2",
+      "#icon2",
+      "#temp-city2",
+      HOME_CITY2_TIMEZONE
+    );
+    this.loadWeather(
+      HOME_CITY3,
+      "#title-city3",
+      "#icon3",
+      "#temp-city3",
+      HOME_CITY3_TIMEZONE
+    );
   }
 
   loadWeather(city, whereTitle, whereIcon, whereTemp, timeZone) {
     $(whereTitle).html(city);
     httpServ.getCurrent(city, (json) => {
-      $(whereTemp).html(Math.round(json.current.temp_c) + '°C' +  ' / ' + this.getActualTime(timeZone));
-      $(whereIcon).attr('src', 'https:'+json.current.condition.icon);
+      $(whereTemp).html(
+        Math.round(json.current.temp_c) +
+          "°C" +
+          " / " +
+          this.getActualTime(timeZone)
+      );
+      $(whereIcon).attr("src", "https:" + json.current.condition.icon);
     });
   }
 
   getActualTime(timeZone) {
-    return new Date().toLocaleString('fr-fr', {timeZone: timeZone}).substring(11,17);
+    let options = {
+      timeZone: timeZone,
+      hour: "numeric",
+      minute: "numeric",
+    },
+    formatter = new Intl.DateTimeFormat([], options);
+    return formatter.format(new Date())
   }
 
   events() {
-    $('#link-station-text-home').click(() => {
-      $('#a-station').click();
+    $("#link-station-text-home").click(() => {
+      $("#a-station").click();
     });
-    $('#weather-1').click(() => {
+    $("#weather-1").click(() => {
       indexCtrl.changeViewToStation(HOME_CITY1);
     });
-    $('#weather-2').click(() => {
+    $("#weather-2").click(() => {
       indexCtrl.changeViewToStation(HOME_CITY2);
     });
-    $('#weather-3').click(() => {
+    $("#weather-3").click(() => {
       indexCtrl.changeViewToStation(HOME_CITY3);
     });
   }
-  
 }
