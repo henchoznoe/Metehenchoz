2d9bf26036977ca16ce75fcbb7dda280869b82fd
HenchozN <nhenchoz122@icloud.com>
2022-06-14 21:29:07 +0200

    Noé

--- js/ctrls/forecastCtrl.js
+++ js/ctrls/forecastCtrl.js
@@ -16,7 +16,6 @@ class ForecastCtrl {
       this.fillForecast1(json);
       this.fillForecast2(json);
       this.fillForecast3(json);
-      console.log(json)
     });
   }
 
@@ -28,7 +27,7 @@ class ForecastCtrl {
     $('#avgTemp-1-forecast').html(json.forecast.forecastday[0].day.avgtemp_c + '°C');
     $('#maxTemp-1-forecast').html(json.forecast.forecastday[0].day.maxtemp_c + '°C');
     for (let i = 0; i < (json.forecast.forecastday[0].hour).length; i++) {
-      $('#div-days-1-forecast').append('<div class="col-xl-1 col-lg-3 col-md-3 col-sm-3 text-center p-3" id="div-hours-'+(i+1)+'-forecast"></div>');
+      $('#div-days-1-forecast').append('<div class="col-xl-1 col-lg-3 col-md-3 col-sm-3 col-6 text-center p-3" id="div-hours-'+(i+1)+'-forecast"></div>');
       $('#div-hours-'+(i+1)+'-forecast').append('<span class="days-hour-1-forecast" id="days-hour-'+(i+1)+'-forecast"></span><br>');
       $('#div-hours-'+(i+1)+'-forecast').append('<img id="days-icon-'+(i+1)+'-forecast"><br>');
       $('#div-hours-'+(i+1)+'-forecast').append('<span class="days-temp-1-forecast" id="days-temp-'+(i+1)+'-forecast">temp</span>');
@@ -42,10 +41,15 @@ class ForecastCtrl {
     $('#date-2-forecast').html(new Date(json.forecast.forecastday[1].date).toDateString());
     $('#sunrise-2-forecast').html(json.forecast.forecastday[1].astro.sunrise);
     $('#sunset-2-forecast').html(json.forecast.forecastday[1].astro.sunset);
+    $('#minTemp-2-forecast').html(json.forecast.forecastday[1].day.mintemp_c + '°C');
+    $('#avgTemp-2-forecast').html(json.forecast.forecastday[1].day.avgtemp_c + '°C');
+    $('#maxTemp-2-forecast').html(json.forecast.forecastday[1].day.maxtemp_c + '°C');
   }
 
   fillForecast3(json) {
     $('#date-3-forecast').html(new Date(json.forecast.forecastday[2].date).toDateString());
+    $('#sunrise-3-forecast').html(json.forecast.forecastday[2].astro.sunrise);
+    $('#sunset-3-forecast').html(json.forecast.forecastday[2].astro.sunset);
   }
 
 }
