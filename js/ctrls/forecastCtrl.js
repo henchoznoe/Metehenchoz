40ae6966ff3506bc3067d52f7e8b556866bffe92
HenchozN <nhenchoz122@icloud.com>
2022-06-14 20:55:20 +0200

    Noé

--- js/ctrls/forecastCtrl.js
+++ js/ctrls/forecastCtrl.js
@@ -28,9 +28,13 @@ class ForecastCtrl {
     $('#avgTemp-1-forecast').html(json.forecast.forecastday[0].day.avgtemp_c + '°C');
     $('#maxTemp-1-forecast').html(json.forecast.forecastday[0].day.maxtemp_c + '°C');
     for (let i = 0; i < (json.forecast.forecastday[0].hour).length; i++) {
-      console.log('ok')
-      $('#div-days-1-forecast').append('<div class="col-lg-1 col-md-3 col-sm-3 text-center">vvv</div>');
-      $('#div-hour-1-forecast').append('<div class="col-lg-1 col-md-3 col-sm-3 text-center">vvvv</div>');
+      $('#div-days-1-forecast').append('<div class="col-xl-1 col-lg-3 col-md-3 col-sm-3 text-center p-3" id="div-hours-'+(i+1)+'-forecast"></div>');
+      $('#div-hours-'+(i+1)+'-forecast').append('<span class="days-hour-1-forecast" id="days-hour-'+(i+1)+'-forecast"></span><br>');
+      $('#div-hours-'+(i+1)+'-forecast').append('<img id="days-icon-'+(i+1)+'-forecast"><br>');
+      $('#div-hours-'+(i+1)+'-forecast').append('<span class="days-temp-1-forecast" id="days-temp-'+(i+1)+'-forecast">temp</span>');
+      $('#days-hour-'+(i+1)+'-forecast').html(json.forecast.forecastday[0].hour[i].time.substring(11,16));
+      $('#days-icon-'+(i+1)+'-forecast').attr('src', json.forecast.forecastday[0].hour[i].condition.icon);
+      $('#days-temp-'+(i+1)+'-forecast').html(Math.round(json.forecast.forecastday[0].hour[i].temp_c) + '°C');
     }
   }
 
