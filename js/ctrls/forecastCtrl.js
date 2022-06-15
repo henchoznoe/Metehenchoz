f2058ade8e347de4950182c0e3b72b09dd7997b8
HenchozN <nhenchoz122@icloud.com>
2022-06-15 10:28:39 +0200

    Noé

--- js/ctrls/forecastCtrl.js
+++ js/ctrls/forecastCtrl.js
@@ -8,33 +8,27 @@ class ForecastCtrl {
 
   constructor(cityEntered) {
     this.cityEntered = cityEntered;
-    this.fillAll(cityEntered);
+    this.fillAllForecast(cityEntered);
   }
 
-  fillAll(cityEntered) {
+  fillAllForecast(cityEntered) {
     httpServ.getForecast(cityEntered, (json) => {
-      this.fillAllForecast(json);
-    });
-  }
-
-  fillAllForecast(json) {
-    for (let i = 1; i <= 3; i++) {
-      $('#date-'+i+'-forecast').html(new Date(json.forecast.forecastday[i-1].date).toDateString());
-      $('#sunrise-'+i+'-forecast').html(json.forecast.forecastday[i-1].astro.sunrise);
-      $('#sunset-'+i+'-forecast').html(json.forecast.forecastday[i-1].astro.sunset);
-      $('#minTemp-'+i+'-forecast').html(json.forecast.forecastday[i-1].day.mintemp_c + '°C');
-      $('#avgTemp-'+i+'-forecast').html(json.forecast.forecastday[i-1].day.avgtemp_c + '°C');
-      $('#maxTemp-'+i+'-forecast').html(json.forecast.forecastday[i-1].day.maxtemp_c + '°C');
-      for (let j = 1; j <= (json.forecast.forecastday[i-1].hour).length; j++) {
-        $('#div-days-'+i+'-forecast').append('<div class="col-xl-1 col-lg-3 col-md-3 col-sm-3 col-6 text-center p-3" id="div-hours-'+i+'-'+j+'-forecast"></div>');
-        $('#div-hours-'+i+'-'+j+'-forecast').append('<span id="days-hour-'+i+'-'+j+'-forecast"></span><br>');
-        $('#div-hours-'+i+'-'+j+'-forecast').append('<img id="days-icon-'+i+'-'+j+'-forecast"><br>');
-        $('#div-hours-'+i+'-'+j+'-forecast').append('<span id="days-temp-'+i+'-'+j+'-forecast">temp</span>');
-        $('#days-hour-'+i+'-'+j+'-forecast').html(json.forecast.forecastday[i-1].hour[j-1].time.substring(11,16));
-        $('#days-icon-'+i+'-'+j+'-forecast').attr('src', json.forecast.forecastday[i-1].hour[j-1].condition.icon);
-        $('#days-temp-'+i+'-'+j+'-forecast').html(Math.round(json.forecast.forecastday[i-1].hour[j-1].temp_c) + '°C');
+      for (let i = 1; i <= 3; i++) {
+        $('#date-'+i+'-forecast').html(new Date(json.forecast.forecastday[i-1].date).toDateString());
+        $('#sunrise-'+i+'-forecast').html(json.forecast.forecastday[i-1].astro.sunrise);
+        $('#sunset-'+i+'-forecast').html(json.forecast.forecastday[i-1].astro.sunset);
+        $('#minTemp-'+i+'-forecast').html(json.forecast.forecastday[i-1].day.mintemp_c + '°C');
+        $('#avgTemp-'+i+'-forecast').html(json.forecast.forecastday[i-1].day.avgtemp_c + '°C');
+        $('#maxTemp-'+i+'-forecast').html(json.forecast.forecastday[i-1].day.maxtemp_c + '°C');
+        for (let j = 1; j <= (json.forecast.forecastday[i-1].hour).length; j++) {
+          $('#div-days-'+i+'-forecast').append('<div class="col-xl-1 col-lg-3 col-md-3 col-sm-3 col-6 text-center p-3" id="div-hours-'+i+'-'+j+'-forecast"></div>');
+          $('#div-hours-'+i+'-'+j+'-forecast').append('<span id="days-hour-'+i+'-'+j+'-forecast"></span><br><img id="days-icon-'+i+'-'+j+'-forecast"><br><span id="days-temp-'+i+'-'+j+'-forecast">temp</span>');
+          $('#days-hour-'+i+'-'+j+'-forecast').html(json.forecast.forecastday[i-1].hour[j-1].time.substring(11,16));
+          $('#days-icon-'+i+'-'+j+'-forecast').attr('src', json.forecast.forecastday[i-1].hour[j-1].condition.icon);
+          $('#days-temp-'+i+'-'+j+'-forecast').html(Math.round(json.forecast.forecastday[i-1].hour[j-1].temp_c) + '°C');
+        }
       }
-    }
+    });
   }
 
 }
